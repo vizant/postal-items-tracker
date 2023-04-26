e55de93c7af7a801c7953d1e0c74a122219938b1
= <=>
2023-04-26 09:30:05 +0400

    Add init script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO post_office (address, index, name) VALUES
+('Novo-Sadovaya, 45, Samara', '443110', 'Post office №1'),
+('Gagarina, 35, Samara', '443079', 'Post office №2'),
+('Osipenko, 41a, Samara', '443082', 'Post office №3'),
+('Sportivnaya, 1, Samara', '443030', 'Post office №4');
