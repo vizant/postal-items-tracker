288687e5e0d4c6abd54daecfd5565ffd7992e9ef
= <=>
2023-04-26 09:22:52 +0400

    Add mail class

---
+++ src/main/java/khozhaev/postalitemstracker/PostalItemsTrackerApplication.java
@@ -0,0 +1,13 @@
+package khozhaev.postalitemstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalItemsTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostalItemsTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/khozhaev/postalitemstracker/PostalItemsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package khozhaev.postalitemstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalItemsTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
